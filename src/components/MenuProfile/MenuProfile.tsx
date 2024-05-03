80f501377ba83ed0a94d6561b0222fbfcf529050
daviport <daviportdev@gmail.com>
2024-05-03 12:02:18 -0300

    adição do esqueleto de divs

--- src/components/MenuProfile/MenuProfile.tsx
+++ src/components/MenuProfile/MenuProfile.tsx
@@ -2,10 +2,37 @@ import { Button } from "@/components/ui/button"
 
 
 
-export const MenuProfile = ({label= "Click me!"}) => {
-    return (
-      <Button variant={"outline"}>{label}</Button>
-    //   <Button variant="outline">Button</Button>
-    );
-  }
-  
+export const MenuProfile = ({ label = "Click me!" }) => {
+  return (
+    <div>
+
+      <div>
+        <div className="border-black">
+          icone avatar
+          <div >
+            <h2 className="font-bold">Nome</h2>
+            <h3>email</h3>
+          </div>
+        </div>
+
+      </div>
+
+      <div>
+        <Button variant={"outline"} className="rounded-full">{label}</Button>
+      </div>
+
+      <div>
+        <Button variant={"outline"}>{label}</Button>
+      </div>
+
+    </div>
+  );
+}
+
+
+
+
+
+
+
+{/* <Button variant="outline">Button</Button> */ }
