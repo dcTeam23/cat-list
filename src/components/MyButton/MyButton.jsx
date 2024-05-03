dd6d0152729409f18db2a78eb4fc5c26b575fd08
João Vitor <jvmnc232@gmail.com>
2024-05-03 16:46:57 -0300

    fix: adjust tailwind config

--- src/components/MyButton/MyButton.jsx
+++ src/components/MyButton/MyButton.jsx
@@ -1,9 +1,17 @@
 import { Button } from "../ui/button";
 
-
-
 export const MyButton = ({label= "Click me!"}) => {
   return (
-    <Button variant={"outline"}>{label}</Button>
+    <div className="flex flex-col gap-2" >
+      <p>Teste Tailwind Aplicado - Variações do Componente Button</p>
+      <div className="flex gap-2 mt-2" >
+      <Button variant={"default"}>{label}</Button>
+      <Button variant={"destructive"}>{label}</Button>
+      <Button variant={"ghost"}>{label}</Button>
+      <Button variant={"link"}>{label}</Button>
+      <Button variant={"outline"}>{label}</Button>
+      <Button variant={"secondary"}>{label}</Button>
+      </div>
+    </div>
   );
 }
