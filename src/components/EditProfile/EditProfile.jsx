be2d2f1e8021e1b38923c8deafa00dc445f80f9f
Rodrigo Neto <rodrigonetogithub@gmail.com>
2024-05-03 15:01:41 +0000

    feature: component in progress..

---
+++ src/components/EditProfile/EditProfile.jsx
@@ -0,0 +1,7 @@
+export default function EditProfile(){
+    return (
+<main>
+    <h1>Hello Mundo!</h1>
+</main>
+    )
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
+import EditProfile from '@/components/EditProfile/EditProfile';
 import React from 'react';
 
 export const HomePage = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      {/* <h1>Home Page</h1> */}
+      <EditProfile />
     </div>
   );
 }
