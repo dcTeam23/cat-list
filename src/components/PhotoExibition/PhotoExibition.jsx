afcdd4588507be98c87323c1217e12ba08474ef4
Janaina Tannus <jmtannus@gmail.com>
2024-05-09 00:28:53 +0000

    criando div com moldura, import e butão de remover imagem

---
+++ src/components/PhotoExibition/PhotoExibition.jsx
@@ -0,0 +1,16 @@
+import { Button } from "../ui/button"
+import { Input } from "../ui/input"
+import {ImageIcon, Trash2} from "lucide-react"
+
+export const PhotoExibition = () => {
+    return(
+       <div className="flex flex-col justify-center items-center gap-6 w-[200px]">
+        <Input id="image-upload" accept="image/*" type="file" className="sr-only rounded-full w-[200px] h-[200px]"/>
+        <label className="flex items-center justify-center w-[180px] h-[180px] rounded-full bg-white border-2 border-gray-100 cursor-pointer" htmlFor="image-upload">
+            <ImageIcon className="text-gray-200"/>
+        </label>
+        <Button variant={"outline"} className="w-[75%] text-red-600"> <Trash2 className="mr-1" size={18}/> Remove photo</Button>
+
+       </div> 
+    )
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,10 @@
 import { MyButton } from "@/components/MyButton/MyButton";
+import { PhotoExibition } from "@/components/PhotoExibition/PhotoExibition";
 
 export const HomePage = () => {
   return (
     <div className="m-2" >
-      <h1>Home Page</h1>
-      <MyButton />
+      <PhotoExibition/>
     </div>
   );
 }
