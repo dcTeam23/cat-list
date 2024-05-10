37f7be311ebdd3c4efd55f277dcceed9d898564c
Gustavo dos Santos Nascimento <74427958+GustavoSantoz@users.noreply.github.com>
2024-05-10 04:48:17 +0000

    remove component from home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,7 @@
-import { CatForm } from "../components/cat-form/catform"
-
 export const HomePage = () => {
   return (
     <div className="m-2" >
       <h1>Home Page</h1>
-      <CatForm></CatForm>
     </div>
   );
 }
