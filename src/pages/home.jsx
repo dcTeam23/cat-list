31eb4d8a1f6eaee93ba441cadf82f91ffe7419a7
Gabriel Collares <98597812+GabrielCollares@users.noreply.github.com>
2024-05-10 14:47:10 +0000

    FIX:  Code Change

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,9 @@
+import { MyButton } from "@/components/MyButton/MyButton";
 export const HomePage = () => {
   return (
     <div className="m-2" >
       <h1>Home Page</h1>
+      <MyButton />
     </div>
   );
 }
