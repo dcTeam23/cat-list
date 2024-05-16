366b71a385fb7283ca890ce1056ea16caf5ddb28
Janaina Tannus <61756665+jmtannus@users.noreply.github.com>
2024-05-16 18:35:10 +0000

    retirando eslint e criando primeiro commit navbar

---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,14 @@
+{/*[Integration]: Chamar o botão Add-a-Cat (que vai acionar o CatDialog) 
+e o ícone Avatar através do Header */}
+import { Add-a-Cat } from "../ui/add-a-cat"
+import { Avatar } from "../ui/avatar"
+
+export const Navbar = () => {
+  return (
+  <div className="">
+  upper Navbar
+  <div></div>
+  <div></div>
+  </div>
+  );
+};
