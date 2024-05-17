b0ab345de2be877b92ecbba11b875214497fd3bb
Mansueto Paiva <mansuetoonofre@gmail.com>
2024-05-17 13:06:41 +0000

    atualizado

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -11,28 +11,14 @@
 
 
 export function Avatar({ imgSrc, descricao, format }) {
-     if (format == "square") {
-     return (
-       <div className="container flex justify-center items-center">
-         <img src={imgSrc} className={`avatar ${format} w-32 h-32 border-3 border-white object-cover`} alt={descricao} />    
-           </div>
-     );
-   }  if (format == "circle") {
-     return (
-       <div className="container flex justify-center items-center">
-     <img src={imgSrc} className={`avatar ${format} w-32 h-32 rounded-full object-cover`} alt={descricao} />
+    
+ return (
+     <div className="container flex justify-center items-center">
+       <img
+         src={imgSrc}
+         className={`avatar ${format === 'square' ? 'square' : 'rounded-full'} w-32 h-32 border-3 border-white object-cover`}
+         alt={descricao}
+       />
      </div>
-     );
-   }
-}
-//** essa sujestão quebra o tailwind */
-// }
-// return (
-//     <div className="container flex justify-center items-center">
-//       <img
-//         src={imgSrc}
-//         className={`avatar ${format === 'square' ? 'square' : 'circle'} w-32 h-32 border-3 border-white object-cover`}
-//         alt={descricao}
-//       />
-//     </div>
-//   );
+   );
+  }  
