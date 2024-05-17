2d07a72f416e4c3ddd87957a6cccfa7536c7abaf
Rodolfo Bertini <rodolfobr@gmail.com>
2024-05-17 15:16:03 +0000

    Remove CheckCircledIcon por enquanto...

--- src/components/Popup/PopUp.jsx
+++ src/components/Popup/PopUp.jsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button"
 import { useToast } from "@/components/ui/use-toast"
 import { Toaster } from "@/components/ui/toaster"
-import { CheckCircledIcon } from '@radix-ui/react-icons';
 
 export const PopUpWithToaster = () => { 
   return <> 
