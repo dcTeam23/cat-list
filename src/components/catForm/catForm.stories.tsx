8dd657cc3d27da5d999d78e27e8a7eb8117b66d5
Gustavo dos Santos Nascimento <74427958+GustavoSantoz@users.noreply.github.com>
2024-05-17 14:08:32 +0000

    Fix: name change

--- src/components/catForm/catForm.stories.tsx
+++ src/components/catForm/catForm.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { CatForm, transformProps } from './catform.tsx';
+import { CatForm, transformProps } from './catForm.tsx';
 
 const meta: Meta<transformProps> = {
   component: CatForm,
